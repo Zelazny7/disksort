3b5037ec582f91fde310968c7534d1351433cb4e
Eric Graves <zelazny7@Erics-MacBook-Pro.local>
2020-07-19 20:14:18 -0500

    using deque instead of list

--- disksort.py
+++ disksort.py
@@ -3,31 +3,19 @@ import queue
 import tempfile
 import glob
 import contextlib
-from typing import Tuple
+from collections import deque
 
 
-class QueueItem():
-    def __init__(self, priority, payload):
-        self.priority = priority
-        self.payload = payload
-    
-    def __lt__(self, other):
-        return self.priority <= other.priority
-    
-    def __iter__(self):
-        return iter((self.priority, self.payload))
-
-
-def yield_100_rows(reader, max_buffer_size=100):
-    buffer = []
+def iter_chunks(reader, max_buffer_size=100):
+    buffer = deque()
     for line in reader:
         if len(buffer) >= max_buffer_size:
             yield buffer
-            buffer.clear()
+            # buffer.clear()
         buffer.append(line)
     yield buffer
 
-with open('data/titanic-big.csv', 'r') as fin:
+with open('data/titanic-small.csv', 'r') as fin:
     next(fin) # skip header
     reader = csv.reader(fin)
 
@@ -36,11 +24,12 @@ with open('data/titanic-big.csv', 'r') as fin:
     # fill the minheap
     q = queue.PriorityQueue()
     with tempfile.TemporaryDirectory() as temp_dir:
-        for i, chunk in enumerate(yield_100_rows(reader, 100000)):
-            # fill the heap
+        for i, chunk in enumerate(iter_chunks(reader, 100000)):
+            # fill the min-heap
             for line in chunk:
                 q.put((float(line[9]), '|'.join(line) + '\n'))
             
+            # empty the min-heap into the output files
             with open(f'{temp_dir}/part{i:04d}.dat', 'a') as fout:
                 while not q.empty():
                     fout.write(q.get()[1])
@@ -58,11 +47,9 @@ with open('data/titanic-big.csv', 'r') as fin:
             readers = []
             for part in parts:
                 reader = csv.reader(stack.enter_context(open(part)), delimiter="|")
-                readers.append(yield_100_rows(reader, 10000))
+                readers.append(iter_chunks(reader, 100))
             
-            ## do everything in here
-            ## read first 10 rows of each file
-            # chunks = [next(reader) for reader in readers]
+            ## read first N rows of each file
             chunks = {}
             for i, reader in enumerate(readers):
                 try:
@@ -80,16 +67,16 @@ with open('data/titanic-big.csv', 'r') as fin:
                     fout.writelines(output_buffer)
                     output_buffer.clear()
 
+                # get index of list with smallest item
+                _, i = q.get()
+                deq: deque = chunks[i]
+                row = deq.popleft()
                 
-                priority, i = q.get()
-                l = chunks[i]
-                
-                row = l.pop(0)
                 # add row to output buffer
                 output_buffer.append('|'.join(row) + '\n')
                 
                 # if list is empty read in another N records
-                if len(l) == 0:
+                if len(deq) == 0:
                     try:
                         chunks[i] = next(readers[i])
                     except:
